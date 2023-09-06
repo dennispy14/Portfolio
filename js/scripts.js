2329e8d0d202a872098b8f0cdcc95d9dda245704
Dennis de Oliveira Py <dennispy14@hotmail.com>
2023-09-06 00:05:31 -0300

    Incluido ids nas sections para ajustes personalizados, Incluiod janelas para cada icone na section habilidades, Alteração nos textos

--- js/scripts.js
+++ js/scripts.js
@@ -105,3 +105,49 @@ var swiper = new Swiper('.swiper-container', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+// Função para abrir a mini janela
+function abrirMiniJanela(num) {
+
+    var miniJanela = document.getElementById('mini-janela');
+
+    var h2 = document.getElementById('tituloSkill')
+    var p = document.getElementById('txtSkill')
+    miniJanela.style.display = 'block';
+
+    switch (num) {
+        case 1:
+            h2.textContent = 'Java';
+            p.textContent = 'Sou proficiente em Java, com experiência específica em desenvolvimento web baseado em Java EE. Minhas habilidades incluem a criação de aplicativos robustos, gerenciamento de dependências, configuração de segurança e implementação de serviços RESTful. Trabalhei em projetos de backend que abrangem desde a criação de APIs até a integração com bancos de dados, utilizando mapeamento objeto-relacional (ORM) com Hibernate e Java Persistence API (JPA).';
+            break;
+        case 2:
+            h2.textContent = 'JavaScript';
+            p.textContent = 'Possuo sólidos conhecimentos em JavaScript e sou capaz de criar aplicativos web interativos e responsivos. Minha experiência inclui o uso de bibliotecas populares como React e Vue.js para criar interfaces de usuário dinâmicas. Além disso, estou familiarizado com os princípios de programação assíncrona e manipulação de eventos para criar experiências de usuário atraentes.';
+            break;
+        case 3:
+            h2.textContent = 'HTML';
+            p.textContent = 'Em HTML sou capaz de criar estruturas semânticas para páginas web. Minha experiência inclui a marcação de conteúdo de maneira organizada e acessível, a incorporação de mídia e a criação de formulários interativos. Estou comprometido em seguir as melhores práticas de HTML para garantir que os sites sejam acessíveis e bem estruturados.';
+            break;
+        case 4:
+            h2.textContent = 'CSS';
+            p.textContent = 'Tenho experiência em estilização de páginas web usando CSS. Isso inclui a criação de layouts responsivos, seletores CSS avançados e animações. Minha habilidade em CSS também se estende à criação de designs acessíveis e compatíveis com vários navegadores para garantir uma experiência consistente do usuário';
+            break;
+        default:
+            h2.textContent = '';
+            p.textContent = '';
+    }
+
+    setTimeout(function () {
+        miniJanela.classList.add('aberto');
+    }, 10); // Pequeno atraso para garantir a transição suave
+}
+// Função para fechar a mini janela
+function fecharMiniJanela() {
+    var miniJanela = document.getElementById('mini-janela');
+    miniJanela.classList.remove('aberto');
+    setTimeout(function () {
+        miniJanela.style.display = 'none';
+    }, 300); // Espere a animação terminar antes de ocultar completamente (corresponde à duração da transição em milissegundos)
+}
+
+
